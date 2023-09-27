d059cdb75aacb9438f45cfe9b7973ef1776ce3c5
geniee1220 <geniee1220@gmail.com>
2023-09-27 17:37:00 +0900

    Design : 메인 페이지 UI 리스트 형식으로 변경

--- components/template/MainTemplate/index.js
+++ components/template/MainTemplate/index.js
@@ -4,18 +4,15 @@ import styles from './MainTemplate.module.css';
 import utilStyles from '../../../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = `Geniee's Memoirey`;
+export const siteTitle = 'Memoirey';
 
 export default function MainTemplate({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="새삼스러운 오늘의 기록 Memoirey" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
