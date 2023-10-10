fdf4aa1f81944790094b995da792ab09a1cde50b
geniee1220 <geniee1220@gmail.com>
2023-10-11 00:07:17 +0900

    Chore : next-sitemap 라이브러리 설치

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://project-memoirey.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
