a321cb7e29be0dd16e97dfafce7a30b5b87c8410
geniee1220 <geniee1220@gmail.com>
2023-10-11 16:35:28 +0900

    Refactor : 댓글 컴포넌트가 포스트 하단에 위치하도록 이동

--- components/template/MainTemplate/index.js
+++ components/template/MainTemplate/index.js
@@ -6,7 +6,6 @@ import Link from 'next/link';
 
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import { useEffect, useState } from 'react';
-import Utterances from '../../Utterances';
 
 const name = `Geniee's Memoirey`;
 export const siteTitle = 'Memoirey';
